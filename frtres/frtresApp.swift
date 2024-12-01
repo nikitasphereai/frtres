9ab687894f776410370f3f24b707f19543d5a482
nikitasphereai <nikita.sphereai@gmail.com>
2024-12-01 21:28:23 +0700

    Initial Commit

---
+++ frtres/frtresApp.swift
@@ -0,0 +1,17 @@
+//
+//  frtresApp.swift
+//  frtres
+//
+//  Created by GRIDFLOW65@GMAIL.COM on 01.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct frtresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
